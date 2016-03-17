08e16692049deb6f5a71132dde9098ac75ff7c76
Jon Rowe <hello@jonrowe.co.uk>
2016-03-18 07:41:30 +1100

    package setup

--- mix.exs
+++ mix.exs
@@ -7,6 +7,14 @@ defmodule Expect.Mixfile do
       version: "0.0.1",
       elixir: "~> 1.2",
       build_per_environment: false,
+      package: package,
+    ]
+  end
+
+  defp package do
+    [
+      description: "Simple descriptive testing framework",
+      files: ["lib", "mix.exs", "README*"],
     ]
   end
 
